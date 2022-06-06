4bab27966dbca464d9e3c6bf1e48679d27d641e8
Matheus Ribeiro <matheusbrdev@gmail.com>
2022-06-06 16:24:09 -0300

    File reading is completed

--- 03_files_and_streams/1_reading_files/2_all_file.php
+++ 03_files_and_streams/1_reading_files/2_all_file.php
@@ -1,1 +1,21 @@
 <?php
+
+$file = fopen('sw-ordem.txt', 'r');
+$fileSize = filesize('sw-ordem.txt');//pega o tamanho em bytes do arquivo
+
+$movies = fread($file, $fileSize);/*essa função recupera o arquivo baseado na
+quantidade de bytes passados no segundo parâmetro. É necessário ter cuidado visto que o arquivo é aberto na memória RAM*/
+
+echo $movies;
+
+fclose($file);
+
+//Outra forma de fazer tudo isso acima:
+
+$movies = file_get_contents('sw-ordem.txt');//Abre o arquivo, pega o conteúdo e fecha.
+
+//Outra forma de fazer, agora recuperando o arquivo em forma de array
+
+$movies = file('sw-ordem.txt');
+
+var_dump($movies);
