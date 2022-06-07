2f7f5ea65026728cc199132d36bdba03b93cbe88
Matheus Ribeiro <matheusbrdev@gmail.com>
2022-06-07 17:28:33 -0300

    Output on console is completed

--- 03_files_and_streams/4_working_on_console/2_output_on_console.php
+++ 03_files_and_streams/4_working_on_console/2_output_on_console.php
@@ -1,1 +1,8 @@
 <?php
+
+/*Para escrever na tela basta abrir um stream da seguinte forma*/
+fwrite(STDOUT, "Olá mundo\n");
+
+//Um modo de ler um arquivo e passar para outro stream sem fazer o processo de resgatar e escrever:
+$courses = fopen('cursos-php.txt', 'r');
+stream_copy_to_stream($courses, STDOUT);
